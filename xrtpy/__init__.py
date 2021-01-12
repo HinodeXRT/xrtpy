fee0190c2e7dfad2e07c3186c5cb7ed58da5da7b
Nick Murphy <namurphy@cfa.harvard.edu>
2021-01-12 12:28:06 -0500

    Use try/except clause when importing version

--- xrtpy/__init__.py
+++ xrtpy/__init__.py
@@ -1,6 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from .version import __version__
+import warnings
+
+try:
+    from .version import __version__
+except (ImportError, ModuleNotFoundError):
+    warnings.warn("version not found.")
+
 from .example_mod import do_primes
 # Then you can be explicit to control what ends up in the namespace,
 __all__ = ['do_primes']
