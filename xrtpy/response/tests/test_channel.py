b19e810454ff28a4fda9ad484e9b4cf092a2aaac
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2021-04-21 11:02:01 -0700

    Test channel name

--- xrtpy/response/tests/test_channel.py
+++ xrtpy/response/tests/test_channel.py
@@ -3,5 +3,8 @@ from xrtpy.response.channel import Channel
 def test_creating_a_channel():
     Channel()
     
-def test_that_fails():
-    assert False
+    
+def test_channel_name():
+    channel_name = "Al-mesh"
+    channel = Channel(channel_name)
+    assert channel.name == channel_name
