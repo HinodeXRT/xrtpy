bdfe047e9975e2f1753b4a94465ff565e2703c59
Nick Murphy <namurphy@cfa.harvard.edu>
2021-05-10 13:53:50 -0400

    Parametrize tests of channel names

--- xrtpy/response/tests/test_channel.py
+++ xrtpy/response/tests/test_channel.py
@@ -1,6 +1,26 @@
+import pytest
+
 from xrtpy.response.channel import Channel
 
-def test_channel_name():
-    channel_name = "Al-mesh"
+channel_names = [
+    "Al-mesh",
+    "Al-poly",
+    "C-poly",
+    "Ti-poly",
+    "Be-thin",
+    "Be-med",
+    "Al-med",
+    "Al-thick",
+    "Be-thick",
+    "Al-poly/Al-mesh",
+    "Al-poly/Ti-poly",
+    "Al-poly/Al-thick",
+    "Al-poly/Be-thick",
+    "C-poly/Ti-poly",
+]
+
+
+@pytest.mark.parametrize("channel_name", channel_names)
+def test_channel_name(channel_name):
     channel = Channel(channel_name)
     assert channel.name == channel_name
