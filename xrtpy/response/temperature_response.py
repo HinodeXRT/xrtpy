f5110bf34bf64d003674ad40aeb9219eb4155442
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2022-03-25 12:49:57 -0700

    Adding temperature response

---
+++ xrtpy/response/temperature_response.py
@@ -0,0 +1,218 @@
+__all__ = [ 
+    "TemperatureResponse"
+    ]
+
+import pkg_resources
+
+import scipy.io
+from scipy import integrate
+from scipy import interpolate
+
+import numpy as np
+from astropy import units as u
+
+from astropy.constants import c, h
+
+from xrtpy.response.channel import Channel,effective_area
+
+_c_Å_per_s = c.to(u.angstrom / u.second).value
+_h_eV_s = h.to(u.eV * u.s).value
+
+_CHIANTI_filename = pkg_resources.resource_filename( "xrtpy","response/data/XRT_emiss_model.default_CHIANTI.geny") 
+_CHIANTI_file = scipy.io.readsav(_CHIANTI_filename)
+_XRT_emiss_model_file = _CHIANTI_file['p0']
+
+
+CHIANTI_file = {
+    "abundant_model":_XRT_emiss_model_file["ABUND_MODEL"][0],
+    "dens_model": _XRT_emiss_model_file["DENS_MODEL"][0],
+    "ioneq_model": _XRT_emiss_model_file["IONEQ_MODEL"][0],  
+    "name" : _XRT_emiss_model_file["NAME"][0],
+    "spectra" : _XRT_emiss_model_file["SPEC"],
+    "spectra_units" : _XRT_emiss_model_file["SPEC_UNITS"][0],
+    "temperature" : _XRT_emiss_model_file["TEMP"][0],
+    "temp_units" : _XRT_emiss_model_file["TEMP_UNITS"][0],
+    "tlength" : _XRT_emiss_model_file["TLENGTH"][0],
+    "wlength" : _XRT_emiss_model_file["WLENGTH"][0],
+    "wavelength" : _XRT_emiss_model_file["WAVE"][0],
+    "wavelength_units" : _XRT_emiss_model_file["WAVE_UNITS"][0],
+}
+
+
+class TemperatureResponse:
+    """Produce the temperature response for each XRT x-ray channel, assuming a spectral emission model."""
+
+    def __init__(self,filter_name, observation_date):
+        self._name = filter_name
+        self._observation_date  = observation_date 
+        self._channel = Channel(self.name)
+        
+    @property
+    def name(self):
+        """Name of searched filter."""
+        return self._name
+
+    @property
+    def observation_date(self):
+        """Users date of observation."""
+        return self._observation_date 
+
+    @property
+    def CHIANTI_version(self):
+        """Name of the emission model."""
+        return CHIANTI_file['name']
+
+    @property
+    def abundant_model(self):
+        """A brief description of what abundance model was used in the creation of the emission spectra."""
+        return CHIANTI_file['abundant_model']
+
+    @property
+    def density_model(self):
+        """A brief description of the plasma density, emission measure,or differential emission measure that was used in the creation of the emission spectra."""
+        return CHIANTI_file['dens_model']
+    
+    @property
+    def ionization_model(self):
+        """A brief description of that ionization equilibrum model was used in the creation of the emission spectra."""
+        return CHIANTI_file['ioneq_model']
+
+    @property
+    @u.quantity_input
+    def CHIANTI_temperature(self):
+        """CHIANTI temperatures in degrees Kelvin,K."""
+        return u.Quantity( CHIANTI_file['temperature']*u.K)
+
+    @property
+    def CHIANTI_file_spectra(self):
+        """CHIANTI file spectra."""
+        return ( CHIANTI_file['spectra'] )
+
+    @property
+    @u.quantity_input
+    def CHIANTI_wavelength(self):
+        """CHIANTI file wavelength values in Angstroms,Å."""
+        return u.Quantity( CHIANTI_file['wavelength'] * u.Angstrom)
+
+    @property
+    @u.quantity_input
+    def channel_wavelength(self):
+        """Array of wavelengths for every X-ray channel in Angstroms,Å."""
+        return u.Quantity((Channel(self.name).wavelength[:3993])*u.photon) #Make note of reason why you're cuting this short
+
+    @property
+    def focal_len(self):
+        """Focal length of the telescope in units of cm."""
+        return( Channel(self.name).geometry.focal_len )
+    
+    @property
+    def ev_pre_electron(self):
+        """Amount of energy it takes to dislodge 1 electron in the CCD."""
+        return (Channel(self.name).ccd.ccd_ev_ore_electron )
+    
+    @property
+    @u.quantity_input
+    def pixel_size(self)-> u.cm:
+        """CCD pixel size. Units coverted from microns to cm."""
+        ccd_pixel_size = Channel(self.name).ccd.ccd_pixel_size
+        return u.Quantity(ccd_pixel_size.value/(1e4)*u.cm)
+    
+    @property
+    @u.quantity_input
+    def solid_angle(self):
+        """This quantity represents the solid angle, which is given in units of steradians over pixel."""
+        return u.Quantity( ( self.pixel_size/self.focal_len )**2  * u.sr / u.pix  )
+
+    
+    @u.quantity_input
+    def spectra(self): 
+        """Interpolation between the spectra wavelength onto the channel wavelength."""
+        spectra_interpolate = []
+        for i in range(0,61):
+            interpolater = interpolate.interp1d( self.CHIANTI_wavelength ,CHIANTI_file['spectra'][0][i],kind='linear')
+            spectra_interpolate.append( interpolater(self.channel_wavelength) )
+        return u.Quantity(spectra_interpolate * (u.photon * u.cm**3 * (1/u.sr) * (1/u.s) * (1/u.Angstrom)) ) 
+
+    def effective_area(self):
+        return (effective_area( self.name, self.observation_date))
+
+    @u.quantity_input
+    def integration(self):
+
+        wavelength = (self.channel_wavelength).value
+        constants = (_c_Å_per_s * _h_eV_s/ self.channel_wavelength).value
+        factors = (self.solid_angle/self.ev_pre_electron).value
+        effective_area = (self.effective_area()).value
+
+        temp_resp_w_u_c = []
+        for i in range(0,61): 
+            temp_resp_w_u_c.append(integrate.simpson( self.spectra()[i] * effective_area * constants * factors, wavelength ))
+        return u.Quantity(temp_resp_w_u_c * ( u.electron * u.cm**5 * (1/u.s) * (1/ u.pix) )) 
+
+    @property
+    @u.quantity_input
+    def ccd_gain_right(self): 
+        """The camera gain has units of [el DN^-1].Gain applied to convert units."""
+        return (Channel(self.name).ccd.ccd_gain_right /u.DN) 
+
+    @u.quantity_input
+    def temperature_response(self):
+        """Apply gain value to the Temperature Response, units DN cm^5 s^-1 pix^-1"""
+        return u.Quantity( self.integration()/self.ccd_gain_right)
+
+
+Filter = "Al-poly"
+Date ="2006-09-22T22:40:45"
+Temp = TemperatureResponse(Filter,Date )
+
+info = Temp.temperature_response()
+print(info)
+
+
+'''
+
+def plotting():
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=(50, 10))
+
+    plt.plot(temperature_CHIANTI_log, temperature_response_log ,color='orange',label='{:}'.format(Filter))
+    plt.plot(temperature_CHIANTI_log, np.log10( TemperatureResponse('Al-poly',"2006-09-22T22:40:45" ).temperature_response().value ) ,color='red',label='{:}'.format('Al-poly'))
+    plt.plot(temperature_CHIANTI_log, np.log10( TemperatureResponse('C-poly',"2006-09-22T22:40:45" ).temperature_response().value ) ,color='green',label='{:}'.format('C-poly'))
+    plt.plot(temperature_CHIANTI_log, np.log10( TemperatureResponse('Ti-poly',"2006-09-22T22:40:45" ).temperature_response().value ) ,color='darkblue',label='{:}'.format('Ti-poly'))
+    plt.plot(temperature_CHIANTI_log, np.log10( TemperatureResponse('Be-thin',"2006-09-22T22:40:45" ).temperature_response().value ) ,color='mediumvioletred',label='{:}'.format('Be-thin'))
+    plt.plot(temperature_CHIANTI_log, np.log10( TemperatureResponse('Be-med',"2006-09-22T22:40:45" ).temperature_response().value ) ,color='crimson',label='{:}'.format('Be-med'))
+    plt.plot(temperature_CHIANTI_log, np.log10( TemperatureResponse('Al-med',"2006-09-22T22:40:45" ).temperature_response().value ) ,color='darkkhaki',label='{:}'.format('Al-med'))
+    plt.plot(temperature_CHIANTI_log, np.log10( TemperatureResponse('Al-thick',"2006-09-22T22:40:45" ).temperature_response().value ) ,color='darkred',label='{:}'.format('Al-thick'))
+    plt.plot(temperature_CHIANTI_log, np.log10( TemperatureResponse('Be-thick',"2006-09-22T22:40:45" ).temperature_response().value ) ,color='midnightblue',label='{:}'.format('Be-thick'))
+
+    plt.title('Observation Date {:}'.format(Date),fontsize = 25)
+    plt.xlabel('Temperature [K]',fontsize = 25)
+    plt.ylabel('[DN cm^5 s^-1 pix^-1]',fontsize=25)
+    plt.legend(fontsize=22)
+
+    plt.xticks(fontsize=25)
+    plt.yticks(fontsize=25)
+    plt.ylim(-28,-24)
+    #plt.ylim(-31,-28)
+
+    plt.grid()
+    plt.show()  
+
+
+
+Filter = "Al-mesh"
+Date ="2006-09-22T22:40:45"
+Temp = TemperatureResponse(Filter,Date )
+
+temperature_response = Temp.temperature_response()
+
+
+
+temperature_response_log = np.log10( (temperature_response.value) )
+temperature_CHIANTI_log = np.log10(CHIANTI_file['temperature'])
+
+#print('Temperature Response - Logged\n',temperature_response)
+
+#print(plotting())
+'''
