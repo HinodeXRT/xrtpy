7705ebfd8d73d451d7c61789aea45fe1dde25007
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2022-03-31 13:32:02 -0700

    Made clarification to the testing script of the temperature response function.

--- xrtpy/response/tests/test_temperature_response.py
+++ xrtpy/response/tests/test_temperature_response.py
@@ -1,16 +1,8 @@
 import pytest
-
-import numpy as np
+import glob
 import pkg_resources
 
-import sunpy
-import sunpy.map
-import sunpy.io.special
-
-from astropy import units as u
 from datetime import datetime 
-import glob
-
 from xrtpy.response.temperature_response import TemperatureResponse
 
 
@@ -22,22 +14,26 @@ def get_IDL_data_files():
 
     return( sorted(filter_data_files) )
 
+
 filenames = get_IDL_data_files()
+
 
 def _IDL_raw_data_list(filename):
 
     with open(filename, "r") as filter_file:
 
-        list_of_lists = []
+        IDL_data_list = []
         for line in filter_file:
             stripped_line = line.strip()
             line_list = stripped_line.split()
-            list_of_lists.append(line_list)
+            IDL_data_list.append(line_list)
     
-    return list_of_lists
+    return IDL_data_list
+
 
 def IDL_test_filter_name(list_of_lists):
     return(str(list_of_lists[1][1]))
+
 
 def IDL_test_date(list_of_lists):
     obs_date = str(list_of_lists[2][1])
@@ -63,28 +59,29 @@ def _IDL_temperature_response_raw_data(filename):
 
     with open(filename, "r") as filter_file:
 
-        list_of_lists = []
+        IDL_data_list = []
         for line in filter_file:
             stripped_line = line.strip()
             line_list = stripped_line.split()
-            list_of_lists.append(line_list)
+            IDL_data_list.append(line_list)
     
-    new_list = []
-    for list_ in list_of_lists[3:]:
-        new_list.extend(list_)
+    new_IDL_data_list = []
+    for list_ in IDL_data_list[3:]:
+        new_IDL_data_list.extend(list_)
     
-    temperature_response = [float(i) for i in new_list]
+    temperature_response = [float(i) for i in new_IDL_data_list]
     
     return(temperature_response)
+
 
 @pytest.mark.parametrize("filename",filenames )
 
 def test_temperature_response(filename,allclose):
 
-    data_list = _IDL_raw_data_list(filename)
+    IDL_data = _IDL_raw_data_list(filename)
    
-    filter_name = IDL_test_filter_name(data_list)
-    filter_obs_date = IDL_test_date(data_list)
+    filter_name = IDL_test_filter_name(IDL_data)
+    filter_obs_date = IDL_test_date(IDL_data)
 
     IDL_temperature_response = _IDL_temperature_response_raw_data(filename)
 
