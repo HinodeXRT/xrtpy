12a05d66a95800f66fd72d3be7d62e284a98edd3
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2022-04-01 11:38:24 -0700

    Add new effective area script and its corresponding attributes.

--- xrtpy/response/__init__.py
+++ xrtpy/response/__init__.py
@@ -1,7 +1,10 @@
 from . import channel
+from . import effective_area
 from . import temperature_response
 from .channel import Geometry, EntranceFilter, Mirror, Filter, CCD, Channel,EffectiveAreaPreparatory
-from .temperature_response import TemperatureResponse
+from .effective_area import EffectiveAreaFundamental
+from .temperature_response import TemperatureResponseFundamental
+
 
 __all__ = [
     "channel",
@@ -12,7 +15,9 @@ __all__ = [
     "CCD",
     "Channel",
     "resolve_filter_name",
+    "EffectiveAreaFundamental",
+    "effective_area",
     "EffectiveAreaPreparatory",
     "temperature_response",
-    "TemperatureResponse",
+    "TemperatureResponseFundamental",
 ]
