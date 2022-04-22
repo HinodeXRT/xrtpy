24c1f5c40832c23fa40c3bf472925f2403c9b0ec
Nick Murphy <namurphy@cfa.harvard.edu>
2022-04-22 00:10:35 -0400

    Ignore genx & geny files

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,8 @@ exclude_patterns = [
     "_links.rst",
     "_substitutions.rst",
     "Thumbs.db",
+    ".*\.genx",  # guessing this needs a Python regular expression
+    ".*\.geny",
 ]
 
 # The suffix(es) of source filenames.
