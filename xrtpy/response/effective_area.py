4ca128c9323d815a4843c18a5463afd7aaa7dd68
Nick Murphy <namurphy@cfa.harvard.edu>
2022-06-17 16:39:30 -0400

    Update xrtpy/response/effective_area.py

--- xrtpy/response/effective_area.py
+++ xrtpy/response/effective_area.py
@@ -232,7 +232,7 @@ class EffectiveAreaFundamental:
             [float(delta_str[i]) for i in range(len(self.n_DEHP_wavelength))]
         )
 
-         # Interpolate so ranges are the same
+        # Interpolate so ranges are the same
         return interpolate.interp1d(self.n_DEHP_wavelength, delta_float)(
             self.n_DEHP_wavelength
         )
