37668a1ebfdb6b696807a66e0e10cfab108936ca
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2022-10-06 17:01:19 -0400

    Correction on unit placement

--- xrtpy/response/channel.py
+++ xrtpy/response/channel.py
@@ -298,13 +298,13 @@ class CCD:
 
     @property
     @u.quantity_input
-    def ccd_gain_left(self) -> u.electron / u.DN:
+    def ccd_gain_left(self) -> (u.electron / u.DN):
         """Gain when reading the left port of the CCD."""
         return u.Quantity(self._ccd_data["GAIN_L"], (u.electron / u.DN))
 
     @property
     @u.quantity_input
-    def ccd_gain_right(self) -> u.electron / u.DN:
+    def ccd_gain_right(self) -> (u.electron / u.DN):
         """Gain when reading the right port of the CCD."""
         return u.Quantity(57.5, (u.electron / u.DN))
 
