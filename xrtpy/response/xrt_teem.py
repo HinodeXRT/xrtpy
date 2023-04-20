e9e4757c53fc67f43ca7909a94bfba1ac6ba55fe
Jonathan Slavin <jslavin@cfa.harvard.edu>
2023-04-20 15:22:08 -0400

    Updated docstring in xrt_teem to reflect the fact that abundance_model is now a parameter

--- xrtpy/response/xrt_teem.py
+++ xrtpy/response/xrt_teem.py
@@ -35,10 +35,9 @@ def xrt_teem(
 
     .. note::
 
-        Currently this program uses the solar spectrum calculated with CHIANTI
-        database ver. 6.0.1 (density: :math:`10^9` cm\ :sup:`-3`\ , ionization
-        equilibrium: ``chianti.ioneq``, abundance: ``sun_coronal_ext``), because this
-        is the only spectrum available in ``xrtpy``. We expect this to change.
+        The program uses solar spectra calculated with CHIANTI database ver.
+        10.0 (density: :math:`10^9` cm\ :sup:`-3`\ , ionization
+        equilibrium: ``chianti.ioneq``)
 
     Parameters:
     -----------
@@ -54,6 +53,12 @@ def xrt_teem(
         |Map| for the second image (must use different filters from the first
         image). The image shape should match that in ``map1``. The same
         considerations apply as for ``map1``.
+
+    abundance_model : string, optional
+        Chianti abundance model for the spectrum assumed when deriving the
+        plasma temperatures and emission measures. The default is coronal
+        abundances. Other currently available choices are ``"photospheric"'' and
+        ''"hybrid"''.
 
     binfac : integer, optional (default = 1)
         spatial binning factor
