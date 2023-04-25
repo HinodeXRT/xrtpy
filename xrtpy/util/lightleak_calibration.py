c12aa201d95608e6e45bd9f3cbd185a1acfdd511
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2023-04-25 13:20:07 -0700

    Testing/creating new script for subtracting light leak from XRT images

---
+++ xrtpy/util/lightleak_calibration.py
@@ -0,0 +1,11 @@
+"""
+Functionality for subtracting light leak (visible stray light) image from XRT synoptic composite images.
+"""
+__all__ = ["lightleak_correction"]
+
+
+def lightleak_correction():
+    r"""
+    Functionality for subtracting light leak (visible stray light) image from XRT synoptic composite images.
+    """
+    return "Testing/creating new script"
