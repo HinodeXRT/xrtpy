45d8f0bd6e66427c54f86a3416d41f506c839711
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2023-04-25 15:42:38 -0700

    Creating a class for light leak functions

--- xrtpy/util/lightleak_calibration.py
+++ xrtpy/util/lightleak_calibration.py
@@ -3,9 +3,28 @@ Functionality for subtracting light leak (visible stray light) image from XRT sy
 """
 __all__ = ["lightleak_correction"]
 
+from pathlib import Path
 
-def lightleak_correction():
-    r"""
-    Functionality for subtracting light leak (visible stray light) image from XRT synoptic composite images.
-    """
-    return "Testing/creating new script"
+_data_file_path = {
+    "lightleak_calibration_data": Path(__file__).parent.absolute()
+    / "data/lightleak_calibration_data"
+}
+
+
+class lightleak_correction:
+    """Functionality for subtracting light leak (visible stray light) image from XRT synoptic composite images."""
+
+    def __init__(self, data_file, in_idx, in_da, abundance_model="coronal"):
+        self._data_file = data_file
+        self._in_idx = in_idx  # Fact check with new function
+        self._in_da = in_da
+
+    @property
+    def index_data(self):
+        """This is the index for a synoptic composite image."""
+        return self.self._in_idx
+
+    @property
+    def index_data_array(self):
+        """This is the 2D data array for the synoptic composite"""
+        return self._in_idx
