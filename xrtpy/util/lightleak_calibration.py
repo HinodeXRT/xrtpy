8009bf7d51570a43a76ac0e1ad63647a4bb01133
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2023-04-27 12:20:08 -0700

    Removing code to start fresh

--- xrtpy/util/lightleak_calibration.py
+++ xrtpy/util/lightleak_calibration.py
@@ -1,30 +1,10 @@
 """
 Functionality for subtracting light leak (visible stray light) image from XRT synoptic composite images.
 """
-__all__ = ["lightleak_correction"]
+# __all__ = ["lightleak_correction"]
 
+import numpy as np
+
+# Import necessary libraries
 from pathlib import Path
-
-_data_file_path = {
-    "lightleak_calibration_data": Path(__file__).parent.absolute()
-    / "data/lightleak_calibration_data"
-}
-
-
-class lightleak_correction:
-    """Functionality for subtracting light leak (visible stray light) image from XRT synoptic composite images."""
-
-    def __init__(self, data_file, in_idx, in_da, abundance_model="coronal"):
-        self._data_file = data_file
-        self._in_idx = in_idx  # Fact check with new function
-        self._in_da = in_da
-
-    @property
-    def index_data(self):
-        """This is the index for a synoptic composite image."""
-        return self.self._in_idx
-
-    @property
-    def index_data_array(self):
-        """This is the 2D data array for the synoptic composite"""
-        return self._in_idx
+from scipy.signal import convolve2d
