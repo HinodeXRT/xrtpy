44a35fd389674bf60538177024fd87711b42497d
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2023-05-12 14:40:14 -0700

    Adding a new function to get IDL data files

--- xrtpy/util/tests/test_xrt_remove_lightleak.py
+++ xrtpy/util/tests/test_xrt_remove_lightleak.py
@@ -1,9 +1,25 @@
-import numpy as np
-
 from pathlib import Path
 from sunpy.map import Map
 
 from xrtpy.util.xrt_remove_lightleak import xrt_remove_lightleak
+
+
+def get_IDL_data_file():
+    """
+    The XRT composite fits file that been lightleak corrected in
+    IDL have been rename to begin with "ll" referring to lightleak.
+    """
+    directory = (
+        Path(__file__).parent.absolute()
+        / "data"
+        / "light_leak_testing_data_files"
+        / "IDL_lightleak_corrected_data_test_files"
+    )
+    data_files = directory.glob("ll_comp_XRT*.fits")
+    return sorted(data_files)
+
+
+IDL_filenames = get_IDL_data_file()
 
 
 def get_observed_data():
@@ -32,4 +48,6 @@ def test_one_case():
     input_map = Map(input_data)
     IDL_map = Map(IDL_data)
     ll_removed_map = xrt_remove_lightleak(input_map)
+    import numpy as np
+
     assert np.allclose(ll_removed_map.data, IDL_map.data)
