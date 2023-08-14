348f08949a7c1c2a927ce472101e15a3591b30bf
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2023-08-14 10:28:49 -0700

    Adding a function for validating users requested observation date

--- xrtpy/util/time.py
+++ xrtpy/util/time.py
@@ -3,8 +3,17 @@ __all__ = [
 ]
 
 import astropy.time
+import datetime
+import os
+import sunpy.io.special
+import sunpy.time
 
 from datetime import timedelta
+
+from xrtpy.response.EffectiveAreaFundamental import (
+    _ccd_contam_filename,
+    _ccd_contamination_file_time,
+)
 
 # Hinode-XRT mission elapsed time "Epoch" is Sept 22, 2006 21:36:00.
 epoch = astropy.time.Time("2006-09-22 21:36:00")
@@ -32,3 +41,38 @@ def xrt_data_time_to_dt(data_time, epoch):
         data_dates_dt.append(epoch + timedelta(0, dt))
         data_dates_seconds.append(float((epoch + timedelta(0, dt)).strftime("%s")))
     return (data_dates_dt, data_dates_seconds)
+
+
+# @observation_date.setter
+def observation_date(date):  # observation_date(self, date
+    """Validating users requested observation date."""
+
+    observation_date = sunpy.time.parse_time(date)
+
+    if observation_date <= epoch:
+        raise ValueError(
+            f"\nInvalid date: {observation_date.datetime}.\n"
+            f"Date must be after {epoch}."
+        )
+
+    modified_time_path = os.path.getmtime(_ccd_contam_filename)
+    modified_time = astropy.time.Time(modified_time_path, format="unix")
+    latest_available_ccd_data = _ccd_contamination_file_time[-1].datetime.strftime(
+        "%Y/%m/%d"
+    )
+    modified_time_datetime = datetime.datetime.fromtimestamp(
+        modified_time_path
+    ).strftime("%Y/%m/%d")
+
+    if observation_date > modified_time:
+        raise ValueError(
+            "\nNo contamination data is presently available for "
+            f"{observation_date.datetime}.\n The latest available data is on "
+            f"{latest_available_ccd_data}.\n Contamination data is "
+            "updated periodically. The last update was on "
+            f"{modified_time_datetime}. If this is more "
+            "than one month ago, please raise an issue at: "
+            "https://github.com/HinodeXRT/xrtpy/issues/new"
+        )
+
+    _observation_date = observation_date
