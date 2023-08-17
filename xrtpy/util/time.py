0ff4775bd663cac000a4bded2340f22375fb1fd1
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2023-08-17 13:48:51 -0700

    Creating a new function, observation_date

--- xrtpy/util/time.py
+++ xrtpy/util/time.py
@@ -3,17 +3,8 @@ __all__ = [
 ]
 
 import astropy.time
-import datetime
-import os
-import sunpy.io.special
-import sunpy.time
 
 from datetime import timedelta
-
-from xrtpy.response.EffectiveAreaFundamental import (
-    _ccd_contam_filename,
-    _ccd_contamination_file_time,
-)
 
 # Hinode-XRT mission elapsed time "Epoch" is Sept 22, 2006 21:36:00.
 epoch = astropy.time.Time("2006-09-22 21:36:00")
@@ -43,37 +34,41 @@ def xrt_data_time_to_dt(data_time, epoch):
     return (data_dates_dt, data_dates_seconds)
 
 
-# @observation_date.setter
-def observation_date(date):  # observation_date(self, date
-    """Validating users requested observation date."""
+# # @observation_date.setter
+def observation_date(data_time):  # date
+    pass
 
-    observation_date = sunpy.time.parse_time(date)
 
-    if observation_date <= epoch:
-        raise ValueError(
-            f"\nInvalid date: {observation_date.datetime}.\n"
-            f"Date must be after {epoch}."
-        )
+# observation_date(self, date
+#     """Validating users requested observation date."""
 
-    modified_time_path = os.path.getmtime(_ccd_contam_filename)
-    modified_time = astropy.time.Time(modified_time_path, format="unix")
-    latest_available_ccd_data = _ccd_contamination_file_time[-1].datetime.strftime(
-        "%Y/%m/%d"
-    )
-    modified_time_datetime = datetime.datetime.fromtimestamp(
-        modified_time_path
-    ).strftime("%Y/%m/%d")
+#     observation_date = sunpy.time.parse_time(date)
 
-    if observation_date > modified_time:
-        raise ValueError(
-            "\nNo contamination data is presently available for "
-            f"{observation_date.datetime}.\n The latest available data is on "
-            f"{latest_available_ccd_data}.\n Contamination data is "
-            "updated periodically. The last update was on "
-            f"{modified_time_datetime}. If this is more "
-            "than one month ago, please raise an issue at: "
-            "https://github.com/HinodeXRT/xrtpy/issues/new"
-        )
+#     if observation_date <= epoch:
+#         raise ValueError(
+#             f"\nInvalid date: {observation_date.datetime}.\n"
+#             f"Date must be after {epoch}."
+#         )
 
-    # self._observation_date = observation_date
-    observation_date
+#     modified_time_path = os.path.getmtime(_ccd_contam_filename)
+#     modified_time = astropy.time.Time(modified_time_path, format="unix")
+#     latest_available_ccd_data = _ccd_contamination_file_time[-1].datetime.strftime(
+#         "%Y/%m/%d"
+#     )
+#     modified_time_datetime = datetime.datetime.fromtimestamp(
+#         modified_time_path
+#     ).strftime("%Y/%m/%d")
+
+#     if observation_date > modified_time:
+#         raise ValueError(
+#             "\nNo contamination data is presently available for "
+#             f"{observation_date.datetime}.\n The latest available data is on "
+#             f"{latest_available_ccd_data}.\n Contamination data is "
+#             "updated periodically. The last update was on "
+#             f"{modified_time_datetime}. If this is more "
+#             "than one month ago, please raise an issue at: "
+#             "https://github.com/HinodeXRT/xrtpy/issues/new"
+#         )
+
+#     # self._observation_date = observation_date
+#     observation_date
