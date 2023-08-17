b2a3d1cf527e8a652c5472e1ac270cea91dd22df
Joy Velasquez <joy.velasquez@cfa.harvard.edu>
2023-08-17 16:10:05 -0700

    Built validating_data_observation_date function to validate the requested observation date against the available data.

--- xrtpy/util/time.py
+++ xrtpy/util/time.py
@@ -3,9 +3,12 @@ __all__ = [
 ]
 
 import astropy.time
+import os
+import scipy.io
 import sunpy.time
 
 from datetime import datetime, timedelta
+from pathlib import Path
 
 # Hinode-XRT mission elapsed time "Epoch" is Sept 22, 2006 21:36:00.
 epoch = astropy.time.Time("2006-09-22 21:36:00")
@@ -80,34 +83,78 @@ def observation_date(data_time: str) -> datetime:
     return observation_date
 
 
-def validating_data_observation_date():
+###############################################################################
+############* Debating if this function belongs here *#########################
+###############################################################################
+
+
+# Absolute path  - Update method
+_ccd_contam_filename = Path(
+    "/Users/jvelasq/Projects/xrtpy/xrtpy/response/data/xrt_contam_on_ccd.geny"
+)
+
+# Read the contamination file
+_ccd_contam_file = scipy.io.readsav(_ccd_contam_filename)
+
+# CCD contam geny files keys for time and date.
+_ccd_contamination_file_time = astropy.time.Time(
+    _ccd_contam_file["p1"], format="utime", scale="utc"
+)
+
+
+def validating_data_observation_date(observation_date):
     """
     Validate the requested observation date against the available data.
 
     This function checks whether there is data available for the requested observation date.
+    If the observation date is later than the last modification date of the contamination data file,
+    it raises a ValueError indicating that no contamination data is available for the requested date.
+
+    Parameters
+    ----------
+    observation_date : `datetime.datetime`
+        The requested observation date to be validated.
+
+    Returns
+    -------
+    observation_date : `datetime.datetime`
+        The validated observation date.
+
+    Raises
+    ------
+    ValueError
+        If the observation date is later than the last modification date of the contamination data file,
+        indicating that no contamination data is available for the requested date.
+
     """
-    pass
+
+    observation_date = observation_date()
+
+    modified_time_path = os.path.getmtime(_ccd_contam_filename)
+    modified_time = astropy.time.Time(modified_time_path, format="unix")
+    latest_available_ccd_data = _ccd_contamination_file_time[-1].datetime.strftime(
+        "%Y/%m/%d"
+    )
+
+    modified_time_datetime = datetime.datetime.fromtimestamp(
+        modified_time_path
+    ).strftime("%Y/%m/%d")
+
+    if observation_date > modified_time:
+        raise ValueError(
+            "\nNo contamination data is presently available for "
+            f"{observation_date.datetime}.\n The latest available data is on "
+            f"{latest_available_ccd_data}.\n Contamination data is "
+            "updated periodically. The last update was on "
+            f"{modified_time_datetime}. If this is more "
+            "than one month ago, please raise an issue at: "
+            "https://github.com/HinodeXRT/xrtpy/issues/new"
+        )
+
+    return observation_date
 
 
-#     modified_time_path = os.path.getmtime(_ccd_contam_filename)
-#     modified_time = astropy.time.Time(modified_time_path, format="unix")
-#     latest_available_ccd_data = _ccd_contamination_file_time[-1].datetime.strftime(
-#         "%Y/%m/%d"
-#     )
-#     modified_time_datetime = datetime.datetime.fromtimestamp(
-#         modified_time_path
-#     ).strftime("%Y/%m/%d")
-
-#     if observation_date > modified_time:
-#         raise ValueError(
-#             "\nNo contamination data is presently available for "
-#             f"{observation_date.datetime}.\n The latest available data is on "
-#             f"{latest_available_ccd_data}.\n Contamination data is "
-#             "updated periodically. The last update was on "
-#             f"{modified_time_datetime}. If this is more "
-#             "than one month ago, please raise an issue at: "
-#             "https://github.com/HinodeXRT/xrtpy/issues/new"
-#         )
-
-#     # self._observation_date = observation_date
-#     observation_date
+###############################################################################
+###############################################################################
+###############################################################################
+##import pdb; pdb.set_trace()
