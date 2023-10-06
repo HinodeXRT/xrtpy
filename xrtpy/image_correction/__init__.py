5b45c127de91fa193089e6a39d4597e4239dacd4
Jonathan Slavin <jslavin@cfa.harvard.edu>
2023-10-06 17:02:06 -0400

    small changes to image_correction/__init__.py

--- xrtpy/image_correction/__init__.py
+++ xrtpy/image_correction/__init__.py
@@ -1,9 +1,12 @@
-from xrtpy.image_correction import xrt_deconvolve, xrt_remove_lightleak
+"""Subpackage for image correction"""
 
 __all__ = [
     "xrt_deconvolve",
     "xrt_remove_lightleak",
 ]
+
+from xrtpy import image_correction
+from xrtpy.image_correction import xrt_deconvolve, xrt_remove_lightleak
 
 _SSW_MIRRORS = [
     "https://sohoftp.nascom.nasa.gov/solarsoft/",
