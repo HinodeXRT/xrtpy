0ac859788a980b006d3fc7d18e46483b2dec63b0
Nick Murphy <namurphy@cfa.harvard.edu>
2024-05-31 16:39:13 -0400

    Revert "Move _SSW_MIRRORS out of __init__.py"
    This reverts commit 2ab203cc3e63eb50c126f5ea76387a1f308c665e.

--- xrtpy/image_correction/__init__.py
+++ xrtpy/image_correction/__init__.py
@@ -4,3 +4,8 @@ __all__ = [
     "deconvolve",
     "remove_lightleak",
 ]
+
+_SSW_MIRRORS = [
+    "https://sohoftp.nascom.nasa.gov/solarsoft/",
+    "https://hesperia.gsfc.nasa.gov/ssw/",
+]
--- xrtpy/image_correction/_ssw_mirrors.py
+++
@@ -1,6 +0,0 @@
-"""Websites that are mirrors of SolarSoft IDL."""
-
-_SSW_MIRRORS = [
-    "https://sohoftp.nascom.nasa.gov/solarsoft/",
-    "https://hesperia.gsfc.nasa.gov/ssw/",
-]
