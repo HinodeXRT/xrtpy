a58f1f4e229cb398aba37876668e0d40b1ee9d8e
Nick Murphy <namurphy@cfa.harvard.edu>
2024-05-31 16:19:06 -0400

    Add imports

--- xrtpy/image_correction/__init__.py
+++ xrtpy/image_correction/__init__.py
@@ -5,6 +5,8 @@ __all__ = [
     "remove_lightleak",
 ]
 
+from xrtpy.image_correction import deconvolve, remove_lightleak
+
 _SSW_MIRRORS = [
     "https://sohoftp.nascom.nasa.gov/solarsoft/",
     "https://hesperia.gsfc.nasa.gov/ssw/",
