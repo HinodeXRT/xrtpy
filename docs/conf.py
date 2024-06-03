e073ab49502cb5e379258e3c54c0bbd0f4b61551
Joy <74623359+joyvelasquez@users.noreply.github.com>
2024-06-03 16:56:49 -0700

    Notebook Guide to Data Extraction (#210)
    * Notebook Guide to Data Extraction
    
    * Update text - added year notebook made
    
    * Adding a new section  Filtering FITS Files Through Selected Filter
    
    * Adding a new section:Visualizing Solar Dynamics: Creating a Movie from XRT FITS Data
    
    * Adding a new section:Filtering FITS Files Through Pixel Size
    
    * Adding a new section:Navigating FITS Images
    
    * Adding a new section: Removing Unwanted FITS Files
    
    * Adding a new section: Filtering FITS Files by Exposure Time
    
    * Reviewing code, markdown text, and comments
    
    * Reviewing code, markdown, and doc-strings.
    
    * Reviewing code, markdown, and doc-strings.Near complete
    
    * Reviewing code and text, ready for review
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updating options install requires - extras
    
    * Updating dependencies
    
    * Updating doc dependencies
    
    * Adding nbsphinx_allow_errors
    
    * Updating markdown, trying to fix build documentation to pass
    
    * organizing sections and subsections in the markdown doc
    
    * Fixing markdown #s
    
    * Fixing markdown headings
    
    * Fixing markdown headings to pass doc test
    
    * Word edit
    
    * Disabling Notebook Execution
    
    * Address RET508 error
    
    * Shorten function to filter_fits_by_exposure_time
    
    * Upating doc build
    
    * Upating titles to pass doc build
    
    * Fixing ruff rule TRY003
    
    * Trying to address ruff error TRY003
    
    * Trying to address ruff error TRY003-2nd try
    
    * Add TRY003 ruff irgnore
    
    * Trying to add a thumbnail
    
    * Adding xrt_example_data_plot function
    
    * Adding hinode png
    
    * Adding hinode png as thumb nail to a notebook: nbsphinx_thumbnails
    
    * Removed plotting function-no need
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,10 @@ source_suffix = ".rst"
 # The root toctree document.
 root_doc = "index"
 
+# -- nbsphinx configuration --------------------------------------------------
+
+nbsphinx_allow_errors = True  # Allow errors in Jupyter notebooks
+
 # -- Options for intersphinx extension ---------------------------------------
 
 intersphinx_mapping = {
@@ -135,6 +139,9 @@ nbsphinx_thumbnails = {
     "notebooks/getting_started/units": (
         "_static/notebook_images/astropy_logo_notext.png"
     ),  # CC BY-SA
+    "notebooks/getting_started/A_Practical_Guide_to_Data_Extraction_and_Visualization": (
+        "_static/notebook_images/hinode_satellite.png"
+    ),
 }
 
 # adapted from sphinx-hoverxref conf.py
