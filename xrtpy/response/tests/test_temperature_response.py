0fdff78c96f5429af73fc52cb6f5c88103f04458
joyvelasquez <joy.velasquez@cfa.harvard>
2025-03-31 14:53:54 -0400

    Drafting final script for TR testing

--- xrtpy/response/tests/test_temperature_response.py
+++ xrtpy/response/tests/test_temperature_response.py
@@ -7,8 +7,6 @@ from astropy.utils.data import get_pkg_data_filenames
 
 from xrtpy.response.temperature_response import TemperatureResponseFundamental
 
-import matplotlib.pyplot as plt
-
 def get_IDL_data_files(abundance):
     filter_data_files = []
     for dir in get_pkg_data_filenames(
@@ -28,9 +26,9 @@ filenames = (
 def test_temperature_response(filename):
     with Path.open(filename) as f:
         _ = f.readline()
-        abundance = f.readline().split()[1]  # e.g. 'coronal'
-        filter_name = f.readline().split()[1]  # e.g. 'Ti_poly'
-        filter_obs_date = " ".join(f.readline().split()[1:])  # e.g. '22 Sep 2015'
+        abundance = f.readline().split()[1]
+        filter_name = f.readline().split()[1]
+        filter_obs_date = " ".join(f.readline().split()[1:])
 
     # The IDL file might say "Sept" instead of "Sep"
     filter_obs_date = filter_obs_date.replace("Sept", "Sep")
@@ -40,32 +38,29 @@ def test_temperature_response(filename):
     IDL_temperature_response = IDL_data[:, 1] * u.Unit("DN cm5 pix-1 s-1")
 
     instance = TemperatureResponseFundamental(
-        filter_name,  # e.g. 'Ti_poly'
-        filter_obs_date,  # e.g. '22 Sep 2015'
-        abundance_model=abundance,  # e.g. 'coronal'
+        filter_name,
+        filter_obs_date,
+        abundance_model=abundance,
     )
-    actual_temperature_response = instance.temperature_response()  # XRTpy
+    
+    actual_temperature_response = instance.temperature_response() 
 
     IDL_temperature_response_interp = np.interp(
-        instance.CHIANTI_temperature.value,  # XRTpy's temperature array
-        IDL_temperature.value,  # IDL's temperature array
-        IDL_temperature_response.value,  # IDL's T-response array
+        instance.CHIANTI_temperature.value,
+        IDL_temperature.value,
+        IDL_temperature_response.value,
     ) * u.Unit("DN cm5 pix-1 s-1")
 
-
+    # NOTE: there may be small deviations where the response function is very small, likely
+    # due to differences in the interpolation schemes. These are not critical as the response
+    # is effectively zero in these regions anyway.
     i_valid = np.where(
         actual_temperature_response > 1e-8 * actual_temperature_response.max()
     )
     
-    ############################# FINAL TESTING #############################
-    # rtol = .025#5e-2#0.1 
-    # atol = 1e-7 * actual_temperature_response.max()
-    rtol =0.01 # 0.005#0.03 ##2e-2#5e-2#0.0275 rtol=5e-2 rtol=3e-2
-    atol = 1e-6 * actual_temperature_response.max()
-
     assert u.allclose(
         actual_temperature_response[i_valid],
         IDL_temperature_response_interp[i_valid],
-        rtol=rtol,
-        atol=atol,
+        rtol=0.005,
+        atol=1e-6 * actual_temperature_response.max(),
     )
