2748943e2eeba43c2d787215a22322058b6e227a
joyvelasquez <joy.velasquez@cfa.harvard>
2025-07-16 17:08:03 -0400

    Add other function

--- xrtpy/util/__init__.py
+++ xrtpy/util/__init__.py
@@ -1,19 +1,18 @@
 from xrtpy.util.filename2repo_path import filename2repo_path
 from xrtpy.util.make_exposure_map import make_exposure_map
 from xrtpy.util.time import epoch
-from .filters import solve_filter_name
+from .filters import solve_filter_name, validate_and_format_filters
 
 __all__ = [
     "epoch",
     "filename2repo_path",
     "make_exposure_map",
     "SSW_MIRRORS",
-    "solve_filter_name"
+    "solve_filter_name",
+    "validate_and_format_filters"
 ]
 
 SSW_MIRRORS = (
     "https://sohoftp.nascom.nasa.gov/solarsoft/",
     "https://hesperia.gsfc.nasa.gov/ssw/",
 )
-
-
