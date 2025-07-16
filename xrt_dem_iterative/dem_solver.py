cdb20bcf18d3186c1b4d075abe7f8f08f13892c3
joyvelasquez <joy.velasquez@cfa.harvard>
2025-07-16 16:52:38 -0400

    Fixing ruff rules

--- xrt_dem_iterative/dem_solver.py
+++ xrt_dem_iterative/dem_solver.py
@@ -1,11 +1,3 @@
 __all__ = [
-#    "",
+    #    "",
 ]
-
-import astropy.time
-import astropy.units as u
-import matplotlib.pyplot as plt
-import numpy as np
-from lmfit import Parameters , minimize
-from scipy.interpolate import interp1d, CubicSpline
-from numpy import trapezoid #np.trapz Deprecation
