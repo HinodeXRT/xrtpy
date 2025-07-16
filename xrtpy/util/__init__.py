d8d7b16f116daa110a675ee706e53f8160cba845
joyvelasquez <joy.velasquez@cfa.harvard>
2025-07-16 17:00:28 -0400

    Updating __init__ for new function

--- xrtpy/util/__init__.py
+++ xrtpy/util/__init__.py
@@ -1,15 +1,19 @@
 from xrtpy.util.filename2repo_path import filename2repo_path
 from xrtpy.util.make_exposure_map import make_exposure_map
 from xrtpy.util.time import epoch
+from .filters import solve_filter_name
 
 __all__ = [
     "epoch",
     "filename2repo_path",
     "make_exposure_map",
     "SSW_MIRRORS",
+    "solve_filter_name"
 ]
 
 SSW_MIRRORS = (
     "https://sohoftp.nascom.nasa.gov/solarsoft/",
     "https://hesperia.gsfc.nasa.gov/ssw/",
 )
+
+
